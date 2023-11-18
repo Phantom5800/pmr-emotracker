d6badbe8a4ee4071cdb7049f664af09834ade652
Phantom <phantom@phantom-games.com>
2023-11-18 11:58:10 -0800

    Pleasant Path

--- scripts/loadlocations.lua
+++ scripts/loadlocations.lua
@@ -1,3 +1,4 @@
 Tracker:AddLocations("locations/GoombaRegion.json")
 Tracker:AddLocations("locations/ToadTown.json")
 Tracker:AddLocations("locations/ToadTownTunnels.json")
+Tracker:AddLocations("locations/PleasantPath.json")
