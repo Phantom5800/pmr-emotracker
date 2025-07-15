cf24ae34b66dcbba389938265d0601cdf8edb3da
Phantom <phantom@phantom-games.com>
2025-07-15 14:10:47 -0700

    Autotracking mod flags through chapter 3

--- scripts/autotracking_location_mapping.lua
+++ scripts/autotracking_location_mapping.lua
@@ -41,4 +41,56 @@ LOCATION_TO_MOD_FLAGS_MAPPING = {
     -- Toad Town Harbor
     ["@Main Gate/Port/Fishmael Letter"]     = {0x1023},
     ["@Main Gate/Port/Trading Event Toad"]  = {0x1024},
+
+    -- Toad Town Tunnels
+    ["@Rip Cheato/Rip Cheato"]  = {0x102C, 0x102D, 0x102E, 0x102F, 0x1030, 0x1031, 0x1032, 0x1033, 0x1034, 0x1035, 0x1036},
+
+    -- Shooting Star Summit
+    ["@Main Gate/Merluvlee's House/Merlow Letter"]   = {0x102B},
+    ["@Main Gate/Merluvlee's House/Merlow Rewards"]  = {0x10A8, 0x10A9, 0x10AA, 0x10AB, 0x10AC, 0x10AD},
+
+    -- Koopa Region
+    ["@West Koopa Village/Bush Right of Tree"]  = {0x1049},
+    ["@West Koopa Village/Mort T. Letter"]      = {0x1037},
+    ["@West Koopa Village/Koover Letter 1"]     = {0x1038},
+    ["@West Koopa Village/Koover Letter 2"]     = {0x1039},
+    ["@West Koopa Village/Shop"]                = {0x103A, 0x103B, 0x103C, 0x103D, 0x103E, 0x103F},
+    ["@East Koopa Village/Far Left Bush"]       = {0x104A},
+    ["@East Koopa Village/Far Right Bush"]      = {0x10A7},
+    ["@Kolorado/Kolorado (Letter)"]             = {0x1042},
+    ["@East Koopa Village/Kooper"]              = {0x1043},
+    ["@Fuzzy Minigame/Minigame"]                = {0x1044},
+
+    -- two Koot favors give two items, one is mod flag, one is game flag ... mark twice off one flag
+    ["@Koopa Koot/Sleepy Sheep"] = {0x1040, 0x1040}, 
+    ["@Koopa Koot/Life Shroom"]  = {0x1041, 0x1041}, 
+
+    -- Koopa Bros Fortress
+    ["@Fortress Entrance/Bomb Jail/Bombette"] = {0x1045},
+
+    -- Mt Rugged
+    ["@Train Station/Top Bush"]                 = {0x1047},
+    ["@Train Station/Parakarry"]                = {0x1048},
+    ["@Train Station/Whacka Screen/Whacka"]     = {0x1046},
+
+    -- Dry Dry Desert
+    ["@Dry Dry Outpost (West)/Desert (Dry Dry Ruins)/Trading Event Toad"]   = {0x1054},
+    ["@Dry Dry Outpost (West)/Desert (Nomadimouse)/Letter"]                 = {0x1055},
+    ["@Dry Dry Outpost (West)/Desert (Oasis)/Lemon Tree"]                   = {0x1056},
+    ["@Dry Dry Outpost (West)/Desert (Oasis)/Lime Tree"]                    = {0x1057},
+
+    -- Dry Dry Outpost
+    ["@Dry Dry Outpost (West)/Little Mouser Letter"]                = {0x104B},
+    ["@Dry Dry Outpost (West)/Shop Items"]                          = {0x104C, 0x104D, 0x104E, 0x104F, 0x1050, 0x1051},
+    ["@Dry Dry Outpost (West)/Dry Dry Outpost (East)/Moustafa"]     = {0x1052},
+    ["@Dry Dry Outpost (West)/Dry Dry Outpost (East)/Mr. E Letter"] = {0x1053},
+
+    -- Boo's Mansion
+    ["@Outside Boo's Mansion/Far Right Bush"]                                                   = {0x1058},
+    ["@Outside Boo's Mansion/Boo's Mansion Foyer/Franky Letter"]                                = {0x1059},
+    ["@Outside Boo's Mansion/Boo's Mansion Foyer/Fake Chest Room/Boo Shop/Igor Letter"]         = {0x105A},
+    ["@Outside Boo's Mansion/Boo's Mansion Foyer/Fake Chest Room/Boo Shop/Shop Items"]          = {0x105B, 0x105C, 0x105D, 0x105E, 0x105F, 0x1060},
+    ["@Outside Boo's Mansion/Boo's Mansion Foyer/Fake Chest Room/Super Boots Room/Big Chest"]   = {0x35},
+    ["@Outside Boo's Mansion/Boo's Mansion Foyer/8bit Mario Room/Crates"]                       = {0x1061, 0x1062},
+    ["@Outside Boo's Mansion/Boo's Mansion Foyer/Bow's Bedroom/Bow"]                            = {0x1063},
 }
